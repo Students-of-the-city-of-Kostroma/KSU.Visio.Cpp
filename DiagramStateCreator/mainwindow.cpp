6f6bd6eaae99c989d66471184ee9574958aaa3ee
Шабров Альберт <albertshabrov720@gmail.com>
2019-12-04 18:15:02 +0300

    add MainWindow::createMenu()

--- DiagramStateCreator/mainwindow.cpp
+++ DiagramStateCreator/mainwindow.cpp
@@ -8,11 +8,30 @@
 ****************************************************************************/
 #include "mainwindow.h"
 
+
 #include <QtWidgets>
+#include <QMenuBar>
 
 MainWindow::MainWindow()
 {
     setWindowTitle(tr("Diagram State Creator"));
     setWindowIcon(QIcon(":/images/logo.png"));
-    setUnifiedTitleAndToolBarOnMac(true);
+    setUnifiedTitleAndToolBarOnMac(true);    
+}
+void MainWindow::CreateMenu()
+{
+   fileMenu = menuBar()->addMenu(tr("&File"));
+   fileMenu ->addAction(createAction);
+   fileMenu ->addAction(openAction);
+   fileMenu ->addAction(saveAction);
+
+   edit = menuBar()->addMenu(tr("&Edit"));
+   edit->addAction(cancelAction);
+   edit->addAction(revertCancelAction);
+
+   about = menuBar()->addMenu(tr("&About"));
+   about->addAction(aboutAction);
+
+   settings = menuBar()->addMenu(tr("&Settings"));
+   settings->addAction(settingsAction);
 }
