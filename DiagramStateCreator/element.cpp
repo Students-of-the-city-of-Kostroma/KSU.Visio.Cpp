6867d22083cbb69a29178b7bf2daacea737a7ba4
alex <gkvalex7@gmail.com>
2019-12-09 04:18:00 +0300

    добавил класс Element

---
+++ DiagramStateCreator/element.cpp
@@ -0,0 +1,14 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the project Diagram State Creator
+**
+****************************************************************************/
+#include "element.h"
+
+Element::Element()
+{
+
+}
---
+++ DiagramStateCreator/element.h
@@ -0,0 +1,19 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the project Diagram State Creator
+**
+****************************************************************************/
+#ifndef ELEMENT_H
+#define ELEMENT_H
+
+
+class Element : public QGraphicsPolygonItem
+{
+public:
+    Element();
+};
+
+#endif // ELEMENT_H
