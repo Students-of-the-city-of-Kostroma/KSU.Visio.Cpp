978d681fc42f1678927c686507de180a3680ae89
Llouqs <enot2532@gmail.com>
2019-12-14 15:15:49 +0300

    Create canvas.h

---
+++ canvas.h
@@ -0,0 +1,59 @@
+#ifndef CANVAS_H
+#define CANVAS_H
+
+// Qt includes
+#include <QTableWidgetItem>
+#include <QGraphicsView>
+#include <QGraphicsSceneDragDropEvent>
+#include <QGraphicsSceneMouseEvent>
+#include <QMimeData>
+#include <QKeyEvent>
+
+// Local includes
+#include "pin.h"
+#include "gates.h"
+#include "componentstab.h"
+#include "canvasmanager.h"
+
+namespace Logicsim
+{
+
+class Canvas : public QGraphicsScene
+{
+    Q_OBJECT
+
+public:
+    Canvas(QObject *parent = nullptr);
+    ~Canvas();
+
+    QGraphicsView* view();
+
+    int tabIndex() const;
+    void setTabIndex(int index);
+
+    CanvasManager *canvasManager();
+    void setManager(CanvasManager* manager);
+
+protected:
+    void dropEvent(QGraphicsSceneDragDropEvent * event);
+    void dragEnterEvent(QGraphicsSceneDragDropEvent * event);
+    void dragMoveEvent(QGraphicsSceneDragDropEvent * event);
+    void dragLeaveEvent(QGraphicsSceneDragDropEvent * event);
+
+    void mousePressEvent(QGraphicsSceneMouseEvent * event);
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent * event);
+
+    void keyPressEvent(QKeyEvent *event);
+
+    void drawBackground(QPainter *painter, const QRectF &rect);
+
+private:
+    class Private;
+    Private* const d;
+};
+
+} // namespace Logicsim
+
+#endif // CANVAS_H
+
