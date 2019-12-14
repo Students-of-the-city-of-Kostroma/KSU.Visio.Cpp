9e0564734708b5cdcd9e4b88086c892cd937a276
Яркин Антон <55685055+Silumgaaar@users.noreply.github.com>
2019-12-14 17:52:39 +0300

    Create logicsim_global.h

---
+++ logicsim_global.h
@@ -0,0 +1,27 @@
+#ifndef LOGICSIM_GLOBAL
+#define LOGICSIM_GLOBAL
+
+// Qt includes
+
+#include <QHash>
+
+// Local includes
+
+#include "gates.h"
+
+namespace Logicsim
+{
+
+/* Drawing constants */
+const int GATE_X_MARGIN = 20;
+const int GATE_Y_MARGIN = 5;
+const int CANVAS_WIDTH = 2250;
+const int CANVAS_HEIGHT = 2250;
+const int GRID_STEP = 150;
+const int NUMBER_OF_SQUARES_IN_ROW = CANVAS_WIDTH / GRID_STEP;
+const int NUMBER_OF_SQUARES_IN_COLUMN = CANVAS_HEIGHT / GRID_STEP;
+
+} // namespace Logicsim
+
+#endif // LOGICSIM_GLOBAL
+
