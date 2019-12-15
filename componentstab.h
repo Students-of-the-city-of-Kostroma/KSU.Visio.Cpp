48ac55821ebe7dac28f8ebefcec69783d2c46a04
Яркин Антон <55685055+Silumgaaar@users.noreply.github.com>
2019-12-15 22:59:16 +0300

    Create componentstab.h

---
+++ componentstab.h
@@ -0,0 +1,39 @@
+#ifndef COMPONENTSTAB_H
+#define COMPONENTSTAB_H
+
+// Qt includes
+#include <QTableWidget>
+#include <QDebug>
+#include <QMimeData>
+#include <QHeaderView>
+
+// Local includes
+
+#include "logicsim_global.h"
+#include "gates.h"
+#include "inputoutputcomponents.h"
+
+namespace Logicsim
+{
+
+class ComponentsTab : public QTableWidget
+{
+    Q_OBJECT
+
+public:
+    ComponentsTab(QWidget* parent = nullptr);
+    ~ComponentsTab();
+
+    QList<Component *> &componentsCollection();
+
+protected:
+    QMimeData* mimeData(const QList<QTableWidgetItem *> items) const;
+
+private:
+    QList<QTableWidgetItem*> m_items;
+    QList<Component*> m_componenets;
+};
+
+} // namespace Logicsim
+
+#endif // COMPONENTSTAB_H
