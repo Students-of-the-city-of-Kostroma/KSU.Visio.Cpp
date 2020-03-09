f89215d286163bfcd54a5e383c222225a7bfb793
Tyson44-rus <55685708+Tyson44-rus@users.noreply.github.com>
2020-03-09 22:52:56 +0300

    Create PaiCircle.cs

---
+++ PaiCircle.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Library
+{
+	/// <summary>
+	/// Класс "Начало процесса"
+	/// </summary>
+	public class PaiCircle
+	{
+		PictureBox picture = new PictureBox();
+		public PaiCircle(PictureBox picture)
+		{
+			this.picture = picture;
+		}
+		/// <summary>
+		/// Метод для рисования "Начало процесса"
+		/// </summary>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		public PictureBox Ris(MouseEventArgs e)
+		{
+			int x1 = e.X;
+			int y1 = e.Y;
+			Image newImage = Library.Properties.Resources.Nachalo_protsessa;
+			Point ulCorner = new Point(x1 - 13, y1);
+			Graphics gr = picture.CreateGraphics();
+			gr.DrawImage(newImage, ulCorner);
+			gr.Dispose();
+			return picture;
+		}
+		/// <summary>
+		/// Метод для перерисовки "Начало процесса"
+		/// </summary>
+		/// <param name="e"></param>
+		/// <returns></returns>
+		public PictureBox Paint(MouseEventArgs e)
+		{
+			int x1 = e.X;
+			int y1 = e.Y;
+			Image newImage = Library.Properties.Resources.Nachalo_protsessa;
+			Point ulCorner = new Point(x1 - 13, y1);
+			Graphics gr = picture.CreateGraphics();
+			gr.DrawImage(newImage, ulCorner);
+			gr.Dispose();
+			return picture;
+		}
+	}
+}
