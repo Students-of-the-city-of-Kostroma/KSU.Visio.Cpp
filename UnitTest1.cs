8df24d7c091594ba2f2b192326ac16749a6cecda
Ekaterina4863 <55685946+Ekaterina4863@users.noreply.github.com>
2020-05-06 12:32:05 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Course_Project;
+using System.Windows.Forms;
+using Library;
+
+namespace UTCourseProject
+{
+	[TestClass]
+	public class UnitTest
+	{
+		[TestMethod]
+		public void Task_49_1()
+		{
+			Saved saved = new Saved();
+			MouseEventArgs Ef = null;
+			int a =saved.Ellipse.Count;
+			saved.Ellipse.Add(Ef);
+			//assert
+			Assert.AreEqual(a+1 ,saved.Ellipse.Count);
+		}
+		[TestMethod]
+		public void Task_49_2()
+		{
+			Saved saved = new Saved();
+			MouseEventArgs Ef = null;
+			int a = saved.Rechteck.Count;
+			saved.Rechteck.Add(Ef);
+			//assert
+			Assert.AreEqual(a + 1, saved.Rechteck.Count);
+		}
+		[TestMethod]
+		public void Task_49_3()
+		{
+			Saved saved = new Saved();
+			MouseEventArgs Ef = null;
+			int a = saved.Arrow.Count;
+			saved.Arrow.Add(Ef);
+			//assert
+			Assert.AreEqual(a + 1, saved.Arrow.Count);
+		}
+		[TestMethod]
+		public void Task_49_7()
+		{
+			Saved saved = new Saved();
+			MouseEventArgs Ef = null;
+			int a = saved.Actor.Count;
+			saved.Actor.Add(Ef);
+			//assert
+			Assert.AreEqual(a + 1, saved.Actor.Count);
+		}
+		[TestMethod]
+		public void Task_49_8()
+		{
+			Saved saved = new Saved();
+			MouseEventArgs Ef = null;
+			int a = saved.DArrow.Count;
+			saved.DArrow.Add(Ef);
+			//assert
+			Assert.AreEqual(a + 1, saved.DArrow.Count);
+		}
+	}
+}
